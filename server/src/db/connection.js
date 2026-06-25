8890cae2500411e5b2c307e45ae33f3f249f4a7c
Amy <amyruby2020@gmail.com>
2026-06-25 17:06:33 +0000

    feat: ETG2-60 configure MongoDB database connection

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  const uri = process.env.MONGO_URI;
-  await mongoose.connect(uri);
+async function connectDB() {
+  const mongoUri = process.env.MONGO_URI;
+
+  if (!mongoUri) {
+    throw new Error('MONGO_URI is missing. Add it to your .env file.');
+  }
+
+  if (mongoose.connection.readyState === 1) {
+    return mongoose.connection;
+  }
+
+  await mongoose.connect(mongoUri);
+
   console.log('MongoDB connected');
-};
+  return mongoose.connection;
+}
 
 module.exports = connectDB;
